58324846e2c863b132587d78184988917eef7a57
Wakim Jraige <wakim.jraige@gmail.com>
2015-05-01 15:50:59 -0300

    Finalizacao da implementacao do sample

--- app/src/main/java/br/com/wakim/vitamiosample/MainActivity.java
+++ app/src/main/java/br/com/wakim/vitamiosample/MainActivity.java
@@ -1,11 +1,15 @@
 package br.com.wakim.vitamiosample;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import io.vov.vitamio.LibsChecker;
 
@@ -13,6 +17,7 @@ import io.vov.vitamio.LibsChecker;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 	EditText mURI;
+	Spinner mSpinner;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +29,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		}
 
 		mURI = (EditText) findViewById(R.id.edit_uri);
+		mSpinner = (Spinner) findViewById(R.id.spinner);
 
 		findViewById(R.id.btn_open).setOnClickListener(this);
+
+		configureSpinner();
+	}
+
+	void configureSpinner() {
+		mSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, getResources().getStringArray(R.array.rmtp_uris)) {
+			@Override
+			public boolean isEnabled(int position) {
+				return !isGroup(position);
+			}
+
+			@Override
+			public View getDropDownView(int position, View convertView, ViewGroup parent) {
+				TextView view = (TextView) super.getDropDownView(position, convertView, parent);
+
+				if (isGroup(position)) {
+					view.setTextColor(getResources().getColor(R.color.colorAccent));
+				} else {
+					view.setTextColor(getResources().getColor(R.color.textColor));
+				}
+
+				return view;
+			}
+
+			boolean isGroup(int position) {
+				return isGroup(getItem(position));
+			}
+
+			boolean isGroup(String item) {
+				return !item.startsWith("rtmp");
+			}
+		});
 	}
 
 	@Override
@@ -33,11 +71,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		if(v.getId() == R.id.btn_open) {
 			String uriText = mURI.getText().toString();
 
-			try {
-				Uri uri = Uri.parse(uriText);
-			} catch(Exception e) {
-				Toast.makeText(this, "URI Invalida!", Toast.LENGTH_LONG).show();
+			if(! uriText.isEmpty()) {
+				playVideo(uriText);
+			} else {
+				String selectedUri = (String) mSpinner.getSelectedItem();
+				playVideo(selectedUri);
 			}
 		}
+	}
+
+	public void playVideo(String uriText) {
+		Uri uri = Uri.parse(uriText);
+		Intent i = new Intent(this, PlayerActivity.class)
+					.putExtra(PlayerActivity.URI_EXTRA, uri);
+
+		startActivity(i);
 	}
 }
---
+++ app/src/main/java/br/com/wakim/vitamiosample/PlayerActivity.java
@@ -0,0 +1,263 @@
+package br.com.wakim.vitamiosample;
+
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import io.vov.vitamio.LibsChecker;
+import io.vov.vitamio.MediaPlayer;
+
+/**
+ * Created by wakim on 01/05/15.
+ */
+public class PlayerActivity extends AppCompatActivity implements SurfaceHolder.Callback, MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnVideoSizeChangedListener, MediaPlayer.OnErrorListener {
+
+	private static final String TAG = "MediaPlayerDemo";
+
+	public static final String URI_EXTRA = PlayerActivity.class.getPackage().getName().concat("URI");
+
+	boolean mIsVideoSizeKnown = false;
+	boolean mIsVideoReadyToBePlayed = false;
+
+	int mVideoWidth;
+	int mVideoHeight;
+
+	int mRawViewWidth;
+	int mRawViewHeight;
+
+	MediaPlayer mMediaPlayer;
+	SurfaceView mPreview;
+	SurfaceHolder mHolder;
+
+	ProgressBar mProgress;
+
+	Uri mStreamingUri;
+
+	int mCurrentOrientation;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if (!LibsChecker.checkVitamioLibs(this)) {
+			return;
+		}
+
+		setContentView(R.layout.activity_player);
+
+		mPreview = (SurfaceView) findViewById(R.id.surface);
+		mProgress = (ProgressBar) findViewById(R.id.progress_bar);
+
+		mProgress.setIndeterminate(true);
+
+		mHolder = mPreview.getHolder();
+		mHolder.addCallback(this);
+		mHolder.setFormat(PixelFormat.RGBA_8888);
+
+		mStreamingUri = getIntent().getExtras().getParcelable(URI_EXTRA);
+
+		if(mStreamingUri == null) {
+			Toast.makeText(this, "URI nao definida!", Toast.LENGTH_LONG).show();
+			finish();
+		}
+
+		mCurrentOrientation = getResources().getConfiguration().orientation;
+
+		showProgress();
+	}
+
+	void showProgress() {
+		mProgress.setVisibility(View.VISIBLE);
+		Log.d(TAG, "SHOW");
+	}
+
+	void hideProgress() {
+		mProgress.setVisibility(View.GONE);
+		Log.d(TAG, "HIDE");
+	}
+
+	void playVideo() {
+		doCleanUp();
+
+		try {
+			mMediaPlayer = new MediaPlayer(this);
+			mMediaPlayer.setDataSource(mStreamingUri.toString());
+			mMediaPlayer.setDisplay(mHolder);
+			mMediaPlayer.prepareAsync();
+			mMediaPlayer.setOnBufferingUpdateListener(this);
+			mMediaPlayer.setOnCompletionListener(this);
+			mMediaPlayer.setOnPreparedListener(this);
+			mMediaPlayer.setOnErrorListener(this);
+			mMediaPlayer.setOnVideoSizeChangedListener(this);
+
+			setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		} catch (Exception e) {
+			Log.e(TAG, "error: " + e.getMessage(), e);
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		releaseMediaPlayer();
+		doCleanUp();
+	}
+
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		releaseMediaPlayer();
+		doCleanUp();
+	}
+
+	private void releaseMediaPlayer() {
+		if (mMediaPlayer != null) {
+			mMediaPlayer.release();
+			mMediaPlayer = null;
+		}
+	}
+
+	private void doCleanUp() {
+		mVideoWidth = 0;
+		mVideoHeight = 0;
+		mIsVideoReadyToBePlayed = false;
+		mIsVideoSizeKnown = false;
+	}
+
+	private void startVideoPlayback() {
+		mHolder.setFixedSize(mVideoWidth, mVideoHeight);
+		mMediaPlayer.start();
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		playVideo();
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {}
+
+	@Override
+	public void onBufferingUpdate(MediaPlayer mp, int percent) {
+	}
+
+	@Override
+	public void onCompletion(MediaPlayer mp) {
+	}
+
+	@Override
+	public void onPrepared(MediaPlayer mp) {
+		mIsVideoReadyToBePlayed = true;
+
+		if (mIsVideoSizeKnown) {
+			startVideoPlayback();
+		}
+
+		hideProgress();
+	}
+
+	@Override
+	public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+		if (width <= 0 || height <= 0) {
+			Log.e(TAG, "invalid video width(" + width + ") or height(" + height + ")");
+			return;
+		}
+
+		mRawViewHeight = height;
+		mRawViewWidth = width;
+
+		mIsVideoSizeKnown = true;
+
+		scalePreviewWithAspectRatio(width, height);
+
+		if (mIsVideoReadyToBePlayed) {
+			startVideoPlayback();
+		}
+	}
+
+	void scalePreviewWithAspectRatio(int sourceWidth, int sourceHeight) {
+		Point windowSize = getAvailableSize();
+		Point scaledSize = scaleWithAspectRatio(sourceWidth, sourceHeight, windowSize.x, windowSize.y);
+
+		mVideoWidth = scaledSize.x;
+		mVideoHeight = scaledSize.y;
+
+		Log.d(TAG, "Scaling video for " + mVideoWidth + ", "  + mVideoHeight);
+
+		ViewGroup.LayoutParams lp = mPreview.getLayoutParams();
+
+		lp.width = mVideoWidth;
+		lp.height = mVideoHeight;
+
+		mPreview.requestLayout();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+
+		if(mCurrentOrientation != newConfig.orientation) {
+			mCurrentOrientation = newConfig.orientation;
+			scalePreviewWithAspectRatio(mRawViewWidth, mRawViewHeight);
+		}
+	}
+
+	public Point getAvailableSize() {
+		View content = findViewById(android.R.id.content);
+
+		int width = content.getWidth();
+		int height = content.getHeight();
+
+		if(mCurrentOrientation == Configuration.ORIENTATION_PORTRAIT) {
+			if(width > height) {
+				int tWidth = width;
+
+				width = height;
+				height = tWidth;
+			}
+		} else if(mCurrentOrientation == Configuration.ORIENTATION_LANDSCAPE){
+			if(height > width) {
+				int tWidth = width;
+
+				width = height;
+				height = tWidth;
+			}
+		}
+
+		return new Point(width, height);
+	}
+
+	public Point scaleWithAspectRatio(double sourceWidth, double sourceHeight, double destWidth, double destHeight) {
+		Point size = new Point();
+
+		double scaleHeight = destHeight / sourceHeight;
+		double scaleWidth = destWidth / sourceWidth;
+		double scale = scaleHeight > scaleWidth ? scaleWidth : scaleHeight;
+
+		size.set((int) (sourceWidth * scale), (int) (sourceHeight * scale));
+
+		return size;
+	}
+
+	@Override
+	public boolean onError(MediaPlayer mp, int what, int extra) {
+		hideProgress();
+		Toast.makeText(this, "Erro ao abrir Stream", Toast.LENGTH_LONG).show();
+		return true;
+	}
+}
